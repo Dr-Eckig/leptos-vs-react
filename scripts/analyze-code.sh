3fa4465e3a62d8708a1a981d120743adfcbbff68
Jessica <schachjessica@gmail.com>
2026-07-15 21:53:15 +0200

    Added script to run code analysation with lizard.

--- scripts/analyze-code.sh
+++ scripts/analyze-code.sh
@@ -1,4 +1,17 @@
 set -e
 
-cd ../shared
-python3 ./decision-density.py
+python3 -m lizard \
+  -l rust \
+  ../leptos-kanban/src \
+  -i -1 \
+  -o ../leptos-complexity.html
+
+python3 -m lizard \
+  -l typescript \
+  -l tsx \
+  ../react-kanban/src \
+  -i -1 \
+  -o ../react-complexity.html
+
+# explorer.exe react-complexity.html
+# explorer.exe leptos-complexity.html
